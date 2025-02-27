2f9ca64664c07b721ee5d9c6abf692dfc8cd7839
gabrielbg02 <gabrielbg0208@gmail.com>
2025-02-27 16:25:27 -0400

    agrego cosas nuevas

---
+++ prueba2.py
@@ -0,0 +1,51 @@
+from io import BytesIO
+import logging
+from PIL import Image
+from inspect import currentframe
+from logging import config
+from uuid import UUID
+import base64
+from fastapi.responses import StreamingResponse, FileResponse
+from fastapi.exceptions import HTTPException
+from classes import ImageData, Settings
+from utils import configure_logging
+
+from tempfile import TemporaryFile
+
+settings = Settings()
+log = logging.getLogger(settings.environment)
+logging.config.dictConfig(configure_logging())
+
+
+async def ctr_get_image_data_from_uuid(uuid: UUID) -> ImageData:
+    log.info(f"Starting {currentframe().f_code.co_name} uuid: {str(uuid)}")
+    image = None
+    try:
+        image = await ImageData.get_image_from_uuid(uuid=uuid)
+    except Exception as e:
+        log.error(f"this is an error ---> {e.__str__()}")
+    finally:
+        log.info(f"{currentframe().f_code.co_name} finish")
+        return image
+
+
+async def ctr_get_image_data_from_filename(filename: str) -> ImageData:
+    log.info(f"Starting {currentframe().f_code.co_name} filename: {str(filename)}")
+    image = await ImageData.get_image_from_filename(filename=filename)
+    if image is None:
+        raise HTTPException(status_code=404, detail="image not found")
+    log.info(f"{currentframe().f_code.co_name} finish")
+    return image
+
+
+async def ctr_stream_image_from_data(data: ImageData):
+    img = Image.open(BytesIO(base64.b64decode(data.imageData, validate=True)))
+    buf = BytesIO()
+    fp = TemporaryFile()
+    img.save(fp, data.imageType)
+    fp.seek(0)
+    return StreamingResponse(
+        fp,
+        media_type=f"image/{data.imageType}".lower()
+    )
+    
